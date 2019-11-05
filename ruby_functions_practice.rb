9844ec055d5216f0ef6ea0dccb1ea2f9ce3bee23
Ellie <cassona08@gmail.com>
2019-11-05 16:03:09 +0000

    adding function to and including length_of_string

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -5,3 +5,19 @@ end
 def add(number_01, number_02)
   number_01 + number_02
 end
+
+def subtract(number_01, number_02)
+  number_01 - number_02
+end
+
+def multiply(number_01, number_02)
+  number_01 * number_02
+end
+
+def divide(number_01, number_02)
+  number_01 / number_02
+end
+
+def length_of_string(test_string)
+test_string.length
+end
